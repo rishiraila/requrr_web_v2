c8c631f660d2e5b562446266c67b7eac42cb7919
rishiraila <rishiraila007@gmail.com>
2025-12-10 16:24:53 +0530

    changgg

--- src/app/utils/notificationService.js
+++ src/app/utils/notificationService.js
@@ -46,7 +46,7 @@ export const sendPushNotification = async ({
     const message = {
       token: fcmToken,
       notification: { title, body },
-      data: Object.fromEntries(Object.entries(data).map(([k, v]) => [k, String(v)])), // all values must be strings
+      data: Object.fromEntries(Object.entries(data).map(([k, v]) => [k, String(v)])), // ll values must be strings
     };
 
     await admin.messaging().send(message);
